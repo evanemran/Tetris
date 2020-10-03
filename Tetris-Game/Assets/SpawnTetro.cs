8a44da0c4324e33fab45a85e6c7e7695e08f9792
Emran Khandaker Evan <evanemran007@gmail.com>
2020-10-03 15:21:47 +0600

    first build
    tetris shapes with dropping and rotationg animation

---
+++ Tetris-Game/Assets/SpawnTetro.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTetro : MonoBehaviour
+{
+    public GameObject[] Tetrominoes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewTetromino();
+    }
+
+    public void NewTetromino()
+    {
+        Instantiate(Tetrominoes[Random.Range(0, Tetrominoes.Length)], transform.position, Quaternion.identity);
+    }
+}
---
+++ Tetris-Game/Assets/TetrisBlock.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+    public Vector3 rotationPoint;
+    private float previousTime;
+    public float fallTime = 0.8f;
+    public static int height = 20;
+    public static int width = 10;
+    public static Transform[,] grid = new Transform[width, height];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if(!ValidMove())
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(1, 0, 0);
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0,0,1), 90);
+            if(!ValidMove())
+            {
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+            }
+
+        }
+
+        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+                AddToGrid();
+                this.enabled = false;
+                FindObjectOfType<SpawnTetro>().NewTetromino();
+            }
+            previousTime = Time.time;
+            
+        }
+    }
+    void AddToGrid()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            grid[roundedX, roundedY] = children;
+        }
+    }
+
+    bool ValidMove()
+    {
+        foreach(Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if(roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+            {
+                return false;
+            }
+            if(grid[roundedX, roundedY] != null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
